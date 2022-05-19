9ff9c441c0cb65e1420a5d2d57bd4e8a4f49bade
ikunalrai@gmail.com <Abacus123#>
2022-05-19 13:49:13 +0530

    ui

--- components/Root.vue
+++ components/Root.vue
@@ -1,9 +1,10 @@
 <template>
-  <div class="container">
+  <div class="">
     <nuxt keep-alive :keep-alive-props="{ max: 10 }" />
     <h1 class="text-center fill-red border-2">This is nuxt js sample.</h1>
 
-    <nav class="bg-purple-800 text-white">
+    <nav class="bg-purple-800 text-white flex justify-between w-auto">
+      <img src="https://randomuser.me/api/portraits/women/12.jpg" class="rounded-3xl h-10 p-0 mt-2 ml-2" alt="" />
       <ul class="px-28 py-4 flex space-x-11 justify-end">
         <li class="cursor-pointer"><a href="/home">Home</a></li>
         <li class="cursor-pointer">About</li>
@@ -13,8 +14,9 @@
         </li>
       </ul>
     </nav>
-
-    <p v-if="$fetchState.pending">Fetching mountains...</p>
+<main>
+  <div class="main bg-slate-100">
+      <p v-if="$fetchState.pending">Fetching mountains...</p>
     <p v-else-if="$fetchState.error">An error occurred :(</p>
     <div v-else>
       <h1>Nuxt Mountains</h1>
@@ -26,6 +28,9 @@
         </div>
       </div>
     </div>
+  </div>
+</main>
+   
   </div>
 </template>
 
